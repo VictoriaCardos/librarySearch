b85ee1626a7aad4565060ceb621a0a0fc6ac6ab9
victoria cardoso <vvjcerque@hotmail.com>
2023-08-20 17:19:39 -0300

    feat: added components

---
+++ library-tech/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
